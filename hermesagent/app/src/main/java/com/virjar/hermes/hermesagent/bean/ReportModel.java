95ccf1486f6e3c469408fd4817adaced7fadba7c
virjar <virjar@virjar.com>
2018-12-04 08:41:57 +0800

    use lombok

--- hermesagent/app/src/main/java/com/virjar/hermes/hermesagent/bean/ReportModel.java
+++ hermesagent/app/src/main/java/com/virjar/hermes/hermesagent/bean/ReportModel.java
@@ -2,10 +2,14 @@ package com.virjar.hermes.hermesagent.bean;
 
 import java.util.Collection;
 
-/**
- * Created by virjar on 2018/8/24.
- */
+import lombok.Data;
 
+/**
+ * Created by virjar on 2018/8/24.<br>
+ * the model report to hermes admin<br>
+ * update on 2018/12/04
+ */
+@Data
 public class ReportModel {
     private String agentServerIP;
     private int agentServerPort;
@@ -16,68 +20,4 @@ public class ReportModel {
     private String systemVersion;
 
     private Collection<String> onlineServices;
-
-    public String getAgentServerIP() {
-        return agentServerIP;
-    }
-
-    public void setAgentServerIP(String agentServerIP) {
-        this.agentServerIP = agentServerIP;
-    }
-
-    public int getAgentServerPort() {
-        return agentServerPort;
-    }
-
-    public void setAgentServerPort(int agentServerPort) {
-        this.agentServerPort = agentServerPort;
-    }
-
-    public double getCpuLoader() {
-        return cpuLoader;
-    }
-
-    public void setCpuLoader(double cpuLoader) {
-        this.cpuLoader = cpuLoader;
-    }
-
-    public double getMemoryInfo() {
-        return memoryInfo;
-    }
-
-    public void setMemoryInfo(double memoryInfo) {
-        this.memoryInfo = memoryInfo;
-    }
-
-    public Collection<String> getOnlineServices() {
-        return onlineServices;
-    }
-
-    public void setOnlineServices(Collection<String> onlineServices) {
-        this.onlineServices = onlineServices;
-    }
-
-    public String getMac() {
-        return mac;
-    }
-
-    public void setMac(String mac) {
-        this.mac = mac;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public String getSystemVersion() {
-        return systemVersion;
-    }
-
-    public void setSystemVersion(String systemVersion) {
-        this.systemVersion = systemVersion;
-    }
 }
